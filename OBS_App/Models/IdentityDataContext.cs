b191a85d7e0322acb00c0e4eb2f2780eb534409e
Muhammed <konya_504_@hotmail.com>
2024-03-05 12:01:58 +0300

    Db contex ayarlandı

--- OBS_App/Models/IdentityDataContext.cs
+++ OBS_App/Models/IdentityDataContext.cs
@@ -2,10 +2,10 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.General;
+using OBS_App.Data;
 
 namespace OBS_App.Models
 {
-
     //VeriTabanı Bağlantı yeri, Veritabanından Role Ve User Bilgilerini Alıyor. Aynı Zamanda DbContext in Üst Sınıfı IdentityDbContext.
     public class IdentityDataContext : IdentityDbContext<AppUser, AppRole, string>
     {
@@ -15,6 +15,19 @@ namespace OBS_App.Models
 
 
         }
+
+        public DbSet<Admin> Adminler { get; set; }
+        public DbSet<AkademikTakvim> AkademikTakvimler { get; set; }
+        public DbSet<Bolum> Bolumler { get; set; }
+        public DbSet<Ders> Dersler { get; set; }
+        public DbSet<Duyuru> Duyurular { get; set; }
+        public DbSet<DuyuruAlici> DuyuruAlicilar { get; set; }
+        public DbSet<Fakulte> Fakulteler { get; set; }
+        public DbSet<FakulteBolum> FakulteBolumler { get; set; }
+        public DbSet<Ogrenci> Ogrenciler { get; set; }
+        public DbSet<OgrenciDers> OgrenciDersler { get; set; }
+        public DbSet<OgrenciMesaj> ogrenciMesajlar { get; set; }
+        public DbSet<Profesor> Profesorler { get; set; }
 
     }
 }
