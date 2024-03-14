90c187fe4b948092c0f63d0f76aef5ea611e341c
KaganMuslu <kaanwasder@gmail.com>
2024-03-14 17:29:55 +0300

    x

--- OBS_App/Areas/Ogrenci/Controllers/OgrenciController.cs
+++ OBS_App/Areas/Ogrenci/Controllers/OgrenciController.cs
@@ -1,15 +1,62 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OBS_App.Models;
 
 namespace OBS_App.Areas.Ogrenci.Controllers
 {
-    [Area("Ogrenci")]
-    [Authorize(Roles = "Ogrenci")]
-    public class OgrenciController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+	[Area("Ogrenci")]
+	[Authorize(Roles = "Ogrenci")]
+	public class OgrenciController : Controller
+	{
+		private readonly IdentityDataContext _context;
+		private readonly UserManager<AppUser> _userManager;
+
+		public OgrenciController(UserManager<AppUser> userManager, IdentityDataContext context)
+		{
+			_userManager = userManager;
+			_context = context;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+
+			var kullanıcı = await _userManager.GetUserAsync(User);
+			if (kullanıcı != null)
+			{
+				// Kullanıcının Danışman Bilgilerini çek
+				var ogrenci = _context.Ogrenciler.FirstOrDefault(d => d.OgrenciEposta == kullanıcı.Email);
+				var danisman = _context.Ogretmenler.FirstOrDefault(x => x.OgretmenAd == ogrenci.OgrenciDanisman);
+
+				return View(danisman);
+			}
+
+			// Hata Gönder
+			return View();
+		}
+
+		public IActionResult DanismanMesaj()
+		{
+			return View();
+		}
+
+		public IActionResult AkademikTakvim()
+		{
+			return View();
+		}
+
+		public IActionResult Duyurular()
+		{
+			var duyurular = _context.Duyurular.ToList();
+			return View(duyurular);
+
+		}
+
+		public IActionResult Ayarlar()
+		{
+			return View();
+		}
+
+	}
 }
