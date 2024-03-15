5a54a4a307d5557d0672c8a7f8e9d3365cedc8b8
KaganMuslu <kaanwasder@gmail.com>
2024-03-15 12:47:56 +0300

    boş

---
+++ OBS_App/Areas/Ogretmen/Controllers/DersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OBS_App.Models;
+
+namespace OBS_App.Areas.Ogretmen.Controllers
+{
+    [Area("Ogretmen")]
+    public class DersController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IdentityDataContext _context;
+
+        public DersController(UserManager<AppUser> userManager, IdentityDataContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Ogretmen_Derslerim()
+        {
+            var kullanıcı = await _userManager.GetUserAsync(User);
+            if (kullanıcı != null)
+            {
+                var ogretmen = _context.Ogretmenler.FirstOrDefault(d => d.OgretmenEposta == kullanıcı.Email);
+                //var dersler = _context.Dersler.Where(x => x.profesorId == ogretmen.Id).ToList();
+                var deneme = _context.Dersler.Include(x => x.Ogretmens).ToList();
+
+                return View(deneme);
+            }
+            return View();
+        }
+
+        public IActionResult DersProgramı()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ OBS_App/Areas/Ogretmen/Controllers/FakulteController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OBS_App.Models;
+
+namespace OBS_App.Areas.Ogretmen.Controllers
+{
+    [Area("Ogretmen")]
+    public class FakulteController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IdentityDataContext _context;
+
+        public FakulteController(UserManager<AppUser> userManager, IdentityDataContext context)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> DanısmanOgrenciler()
+        {
+            var kullanıcı = await _userManager.GetUserAsync(User);
+            if (kullanıcı != null)
+            {
+                var ogretmen = _context.Ogretmenler.FirstOrDefault(x => x.OgretmenEposta == kullanıcı.Email);
+                // Öğretmen İsmine Göre Aramamalı
+                var danisanlar = _context.Ogrenciler.Where(d => d.OgrenciDanisman == ogretmen.OgretmenAd).ToList();
+
+                return View(danisanlar);
+            }
+            return View();
+        }
+
+        public IActionResult AkademikTakvim()
+        {
+            return View();
+        }
+
+
+    }
+}
