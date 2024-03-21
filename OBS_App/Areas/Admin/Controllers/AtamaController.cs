f29d9f08162f334d119dddffeaacb2779ebe16d2
ceyla <ceyla@DESKTOP-F3QPEQJ>
2024-03-21 15:29:03 +0300

    Atama İşlemleri-OgretmenAta.

--- OBS_App/Areas/Admin/Controllers/AtamaController.cs
+++ OBS_App/Areas/Admin/Controllers/AtamaController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OBS_App.Data;
+using OBS_App.Models;
 
 namespace OBS_App.Areas.Admin.Controllers
 {
@@ -8,16 +12,57 @@ namespace OBS_App.Areas.Admin.Controllers
     [Authorize(Roles = "Admin")]
     public class AtamaController : Controller
     {
-        public IActionResult Index()
+        private readonly IdentityDataContext _context;
+
+        public AtamaController(IdentityDataContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> OgretmenAta(int id)
+        {
+            var ders = await _context.Dersler.Include(x => x.Ogretmens).ToListAsync();
+
+            return View(ders);
         }
 
-        public IActionResult DersAtaGuncelle()
+        public async Task<IActionResult> DersAtaGuncelle(int id)
         {
-            return View();
-        }
+            if (id == 0)
+            {
 
+                ViewBag.Ders = new SelectList(_context.Dersler.Where(d => d.OgretmensId == null).ToList(), "Id", "DersAd");
+                ViewBag.Ogretmen = new SelectList(await _context.Ogretmenler.ToListAsync(), "Id", "OgretmenAdSoyad");
+                return View();
+            }
+            else
+            {
+                ViewBag.Ders = new SelectList(_context.Dersler.Where(x => x.Id == id), "Id", "DersAd");
+                ViewBag.Ogretmen = new SelectList(await _context.Ogretmenler.ToListAsync(), "Id", "OgretmenAdSoyad");
+                return View();
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> DersAtaGuncelle(Ders model, int id)
+        {
+
+            var ders = _context.Dersler.FirstOrDefault(a => a.Id == id);
+            if (ders != null)
+            {
+                ders.OgretmensId = model.OgretmensId;
+                _context.Dersler.Update(ders);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("OgretmenAta");
+            }
+            else
+            {
+                ViewBag.Ders = new SelectList(_context.Dersler.ToList(), "Id", "DersAd");
+                ViewBag.Ogretmen = new SelectList(await _context.Ogretmenler.ToListAsync(), "Id", "OgretmenAdSoyad");
+                return View(model);
+            }
+
+
+
+        }
         public IActionResult BaskanAtaGuncelle()
         {
             return View();
